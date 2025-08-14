4d19e4ef1a2f400bcae138352686a9d6bb9b049c
David Rosko <rosko.dav@gmail.com>
2025-08-14 17:46:29 +0200

    Fix references spacing

--- src/components/ClientLogos.tsx
+++ src/components/ClientLogos.tsx
@@ -32,31 +32,31 @@ const ClientLogos = () => {
         </div>
         
         <div className="relative overflow-hidden">
-          <div className="absolute left-0 top-0 bottom-0 w-12 bg-gradient-to-r from-background to-transparent z-10 pointer-events-none"></div>
-          <div className="absolute right-0 top-0 bottom-0 w-12 bg-gradient-to-l from-background to-transparent z-10 pointer-events-none"></div>
+          <div className="absolute left-0 top-0 bottom-0 w-6 sm:w-12 bg-gradient-to-r from-background to-transparent z-10 pointer-events-none"></div>
+          <div className="absolute right-0 top-0 bottom-0 w-6 sm:w-12 bg-gradient-to-l from-background to-transparent z-10 pointer-events-none"></div>
           
-          <div className="flex animate-[scroll_70s_linear_infinite] items-center">
+          <div className="flex animate-[scroll_70s_linear_infinite] will-change-transform items-center">
             {clients.map((client, index) => (
               <div 
                 key={`first-${index}`}
-                className="flex items-center justify-center h-20 w-48 flex-shrink-0 mx-8 group cursor-default"
+                className="flex items-center justify-center h-16 sm:h-20 w-32 sm:w-40 md:w-48 flex-shrink-0 mx-3 sm:mx-6 md:mx-8 group cursor-default"
               >
                 <img 
                   src={client.logo} 
                   alt={client.name}
-                  className="max-h-12 w-auto object-contain opacity-75 group-hover:opacity-100 transition-all duration-300 filter brightness-0 invert-[0.7] group-hover:invert-[0.8]"
+                  className="max-h-10 sm:max-h-12 w-auto object-contain opacity-75 group-hover:opacity-100 transition-all duration-300 filter brightness-0 invert-[0.7] group-hover:invert-[0.8]"
                 />
               </div>
             ))}
             {clients.map((client, index) => (
               <div 
                 key={`second-${index}`}
-                className="flex items-center justify-center h-20 w-48 flex-shrink-0 mx-8 group cursor-default"
+                className="flex items-center justify-center h-16 sm:h-20 w-32 sm:w-40 md:w-48 flex-shrink-0 mx-3 sm:mx-6 md:mx-8 group cursor-default"
               >
                 <img 
                   src={client.logo} 
                   alt={client.name}
-                  className="max-h-12 w-auto object-contain opacity-75 group-hover:opacity-100 transition-all duration-300 filter brightness-0 invert-[0.7] group-hover:invert-[0.8]"
+                  className="max-h-10 sm:max-h-12 w-auto object-contain opacity-75 group-hover:opacity-100 transition-all duration-300 filter brightness-0 invert-[0.7] group-hover:invert-[0.8]"
                 />
               </div>
             ))}
